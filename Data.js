7b5c818ea670a25391e6049128bb028eee3f8360
Antti Hupli <t1huan00@students.oamk.fi>
2023-10-02 20:47:34 +0300

    Toimiva App

---
+++ Data.js
@@ -0,0 +1,77 @@
+export const DATA = [
+    {
+        id: '1',
+        lastname: 'Hupli',
+        firstname: 'Antti'
+    },
+    {
+        id: '2',
+        lastname: 'Markiisi',
+        firstname: 'Milla'
+    },
+    {
+        id: '3',
+        lastname: "Keinonen",
+        firstname: 'Jere'
+    },
+    {
+        id: '4',
+        lastname: "Haavisto",
+        firstname: 'Herman'
+    },
+    {
+        id: '5',
+        lastname: "Maximillian",
+        firstname: 'Didier'
+    },
+    {
+        id: '6',
+        lastname: "Montonen",
+        firstname: 'Miko'
+    },
+    {
+        id: '7',
+        lastname: "Rootbeer",
+        firstname: 'Michael'
+    },
+    {
+        id: '8',
+        lastname: "Mugambe",
+        firstname: 'Kfuvu'
+    },
+    {
+        id: '9',
+        lastname: "Korpela",
+        firstname: 'Jussi'
+    },
+    {
+        id: '10',
+        lastname: "Vepsäläinen",
+        firstname: 'Joel'
+    },
+    {
+        id: '11',
+        lastname: "Laaksonen",
+        firstname: 'Iiro'
+    },
+    {
+        id: '12',
+        lastname: "Kalossi",
+        firstname: 'Klaus'
+    },
+    {
+        id: '13',
+        lastname: "Virta",
+        firstname: 'Pentti'
+    },
+    {
+        id: '14',
+        lastname: "Kuuppa",
+        firstname: 'Pena'
+    },
+    {
+        id: '15',
+        lastname: "Siirappi",
+        firstname: 'Mairea'
+    }
+]
